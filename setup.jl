71ec5ad285f76c1e3e36558584cb9dce6bbd61d6
Rik <rik@huijzer.xyz>
2020-08-13 13:40:05 +0200

    Add setup.jl

---
+++ setup.jl
@@ -0,0 +1,7 @@
+using Pkg
+
+# This file is meant to be used by GitHub Actions.
+
+Pkg.add(PackageSpec(url="https://github.com/rikhuijzer/codex.jl"))
+Pkg.add(PackageSpec(url="https://github.com/rikhuijzer/gendoc.jl"))
+Pkg.add(PackageSpec(url="https://github.com/rikhuijzer/gentex.jl"))
