8a7236002533d84d56a76104f30b368300d2484f
Rik <rik@huijzer.xyz>
2020-08-13 14:43:19 +0200

    Fetch LaTeX via Nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+let
+  inherit (nixpkgs) pkgs;
+  myTex = with pkgs; texlive.combine {
+    inherit (texlive) scheme-basic amsfonts mathtools stmaryrd;
+  };
+
+in pkgs.mkShell {
+  name = "env";
+  buildInputs = with pkgs; [
+    ghostscript
+    imagemagick
+    myTex
+    which
+  ];
+}
