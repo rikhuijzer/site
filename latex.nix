60966933ccd3673dc09799f80fb612db911b0516
Rik <rik@huijzer.xyz>
2020-08-14 21:37:41 +0200

    Make fix

--- latex.nix
+++ latex.nix
@@ -6,12 +6,6 @@ let
     # pgf contains TikZ.
     inherit (texlive) scheme-basic dvisvgm amsfonts mathtools pdfcrop stmaryrd pgf xcolor;
   };
-
-in pkgs.mkShell {
-  name = "env";
-  buildInputs = with pkgs; [
-    ghostscript
-    imagemagick
-    myTex
-  ];
-}
+in [
+  myTex
+]
