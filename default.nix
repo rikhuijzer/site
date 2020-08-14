56be747d301826e3af4666dc953af73b381013ee
Rik <rik@huijzer.xyz>
2020-08-14 10:01:38 +0200

    Add xcolor

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
   inherit (nixpkgs) pkgs;
   myTex = with pkgs; texlive.combine {
     # pgf contains TikZ.
-    inherit (texlive) scheme-basic dvisvgm amsfonts mathtools pdfcrop stmaryrd pgf;
+    inherit (texlive) scheme-basic dvisvgm amsfonts mathtools pdfcrop stmaryrd pgf xcolor;
   };
 
 in pkgs.mkShell {
