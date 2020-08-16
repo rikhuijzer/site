f9d24396ed7556f09fe8600da37a7561db15b8a2
Rik <rik@huijzer.xyz>
2020-08-16 19:04:31 +0200

    Change inherit to import

--- default.nix
+++ default.nix
@@ -2,7 +2,8 @@ let
   # Pinning explicitly to 20.03 to avoid issues with an outdated cache.
   rev = "5272327b81ed355bbed5659b8d303cf2979b6953";
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  inherit (nixpkgs) pkgs;
+  pkgs = import nixpkgs;
+
   myTex = with pkgs; texlive.combine {
     # pgf contains TikZ.
     inherit (texlive) scheme-basic dvisvgm amsfonts mathtools pdfcrop stmaryrd pgf xcolor;
