c92861c3f5f48fcfbcfeda9afa32d41dbe0706ef
Rik <rik@huijzer.xyz>
2020-08-16 19:09:14 +0200

    Fix attempt

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   # Pinning explicitly to 20.03 to avoid issues with an outdated cache.
   rev = "5272327b81ed355bbed5659b8d303cf2979b6953";
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  pkgs = import nixpkgs;
+  pkgs = import nixpkgs {};
 
   myTex = with pkgs; texlive.combine {
     # pgf contains TikZ.
